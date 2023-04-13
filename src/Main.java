c8ed25d26f62a2f629e84caab2dfba519bc39149
bigrusal <bigrusal@yandex.ru>
2023-04-14 07:59:59 +1000

    code updated

--- src/Main.java
+++ src/Main.java
@@ -5,26 +5,26 @@ public class Main {
         // параметров программы: начального счёта,
         // суммы пополнения и тп
 
-        int initial_account = 2200;
-        int refill_amount = 1300;
-        int current_account;
+        int initialAccount = 2200;
+        int refillAmount = 1300;
         int bonus;
+        int currentAccount;
 
         // Условным оператором проверяете, превысила ли
         // сумма пополнения порог, и для этих двух разных
         // сценариев рассчитываете сумму бонуса и выводите
         // на экран.
 
-        if (refill_amount > 1000) {
-            bonus = refill_amount/100;
-            current_account = initial_account + refill_amount + bonus;
+        if (refillAmount > 1000) {
+            bonus = refillAmount / 100;
+            currentAccount = initialAccount + refillAmount + bonus;
             System.out.println("bonus = " + bonus);
-            System.out.println("current account = " + current_account);
+            System.out.println("current account = " + currentAccount);
         } else {
             bonus = 0;
-            current_account = initial_account + refill_amount + bonus;
+            currentAccount = initialAccount + refillAmount + bonus;
             System.out.println("bonus = " + bonus);
-            System.out.println("current account = " + current_account);
+            System.out.println("current account = " + currentAccount);
         }
 
     }
